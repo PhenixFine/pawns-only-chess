9f11fa3fc66f8dd351dd504e3719ee2e3c039e3a
Danny <64429863+PhenixFine@users.noreply.github.com>
2021-08-25 21:36:23 -0400

    Stage 2 of 5 for JetBrains Academy - Kotlin - Pawns-Only Chess project.
    Main.kt changes:
    • main() - with using stored string values and function getString() it prints the name of the game and then asks for the name of the two players. After which it loops asking each of the two players their move until they type exit. If they type an invalid move they are told such, and then the same player is asked again for their move.
    • getString() - prints the given message and returns a line of user input.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,26 @@
 fun main() {
     val chessboard = Chessboard()
+    val gameName = "Pawns-Only Chess"
+    val getName = "Player's name:"
+    val player1 = getString("$gameName\nFirst $getName")
+    val player2 = getString("Second $getName")
+    var currentPlayer = player1
+    var command = ""
+    val getCommand = { command = getString("$currentPlayer's turn:"); command }
 
     chessboard.printChessboard()
+
+    while (getCommand() != "exit") {
+        if (chessboard.validMove(command)) {
+            currentPlayer = if (currentPlayer == player1) player2 else player1
+        } else {
+            println("Invalid Input")
+        }
+    }
+    println("Bye!")
+}
+
+private fun getString(message: String): String {
+    println(message)
+    return readLine()!!
 }
